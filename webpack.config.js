e718b1bad69a8eb6b6b2c530a49bbc570723a24c
bantalek <benjaminolki@gmail.com>
2017-02-13 05:24:19 -0800

    configure webpack for pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -12,59 +12,64 @@
  * 'resolve'
  * Array of file extensions used to resolve modules.
  *
- * 'webpack-dev-server'
- * Is a little node.js Express server, which uses the webpack-dev-middleware to
- * serve a webpack bundle. It also has a little runtime which is connected to
- * the server via Socket.IO.
- *
- * 'webpack/hot/dev-server'
- * By adding a script to your index.html file and a special entry point in your
- * configuration you will be able to get live reloads when doing changes to your
- * files.
- *
  * devtool: 'eval-source-map'
  * http://www.cnblogs.com/Answer1215/p/4312265.html
  * The source map file will only be downloaded if you have source maps enabled
  * and your dev tools open.
  *
- * HotModuleReplacementPlugin()
- * Hot Module Replacement (HMR) exchanges, adds or removes modules while an
- * application is running without page reload.
+ * OccurrenceOrderPlugin
+ * Assign the module and chunk ids by occurrence count. Ids that are used often
+ * get lower (shorter) ids. This make ids predictable, reduces to total file size
+ * and is recommended.
  *
- * NoErrorsPlugin()
- * Hot loader is better when used with NoErrorsPlugin and hot/only-dev-server
- * since it eliminates page reloads altogether and recovers after syntax errors.
+ * UglifyJsPlugin
+ * Minimize all JavaScript output of chunks. Loaders are switched into minimizing mode.
+ *    - 'compress'
+ *      Compressor is a tree transformer which reduces the code size by applying
+ *      various optimizations on the AST.
  *
- * 'react-hot'
- * React Hot Loader is a plugin for Webpack that allows instantaneous live
- * refresh without losing state while editing React components.
+ * 'NODE_ENV'
+ * React relies on process.env.NODE_ENV based optimizations.
+ * If we force it to production, React will get in an optimized manner.
+ * This will disable some checks (eg. property type checks) and give you a smaller
+ *    build and improved performance.
+ *
+ *    Note: That JSON.stringify is needed as webpack will perform string replace
+ *    "as is". In this case we'll want to end up with strings as that's what
+ *    various comparisons expect, not just production. Latter would just cause
+ *    an error.
  *
  * 'babel'
- * Babel enables the use of ES6 today by transpiling your ES6 JavaScript into equivalent ES5 source
- * that is actually delivered to the end user browser.
+ * Babel enables the use of ES6 today by transpiling your ES6 JavaScript into
+ * equivalent ES5 source that is actually delivered to the end user browser.
  */
 
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:5000',
-    'webpack/hot/dev-server',
-    './scripts/index'
-  ],
+  entry: './scripts/index',
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/'
   },
   resolve: {
     extensions: ['', '.js']
   },
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
   ],
   module: {
     loaders: [
