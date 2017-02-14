ee5f8e15f66c955769540c0b1300e19cfc30f26f
bantalek <benjaminolki@gmail.com>
2017-02-13 21:32:20 -0800

    implement suggestions while your search, connected, autocomplete user input in place of searchbox

--- scripts/App.js
+++ scripts/App.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import { InstantSearch, SearchBox, } from 'react-instantsearch/dom';
 import Restaurants from './components/Results.js';
 import SideBar from './components/SideBar.js';
+import AgAutocomplete from 'react-algoliasearch';
+import {connectSearchBox} from 'react-instantsearch/connectors';
+
+const MySearchBox = ({currentRefinement, refine}) => {
+  const suggestionSelected = (event, suggestion, dataset) => {
+    refine(event.target.value);
+  };
+
+  return (<AgAutocomplete
+    apiKey={"d731cdda9f85d927bf8e950389476293"}
+    appId={"FND5QH0PGJ"}
+    displayKey={(hit) => `${hit.name} ${hit.city} ${hit.dining_style}`}
+    indices={[{index: 'restaurants'}]}
+    inputId="input-search"
+    selected={suggestionSelected}
+    placeholder="Search for Restaurants by Name, Cuisine, Location"
+    hitsPerPage={10}
+    updated = {suggestionSelected}
+  />);
+}
+
+const ConnectedSearchBox = connectSearchBox(MySearchBox);
 
 
 export default class App extends Component {
+
   render() {
     return (
       <div className="container">
@@ -14,8 +37,7 @@ export default class App extends Component {
           indexName="restaurants"
           className="container"
         >
-
-          <SearchBox />
+        <ConnectedSearchBox />
           <div className="container content-wrapper">
             <SideBar />
             <Restaurants />
